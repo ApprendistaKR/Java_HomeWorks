97ad482eb4f7416ba7794cf9c54a82243c4a6102
ApprendistaKR <luke1475@naver.com>
2026-09-17 10:56:46 +0900

    exit 입력 전까지 무한으로 계산 가능하게 수정

--- calculator/src/main.java
+++ calculator/src/main.java
@@ -5,30 +5,34 @@ public class main {
 
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("첫 번째 숫자를 입력해주세요.");
-        int num1 = sc.nextInt();
+        String str2 = "";
+        while (!str2.equals("exit")) {
 
-        System.out.print("두 번째 숫자를 입력해주세요.");
-        int num2 = sc.nextInt();
+            System.out.print("첫 번째 숫자를 입력해주세요.");
+            int num1 = sc.nextInt();
 
-        sc.nextLine();
+            System.out.print("두 번째 숫자를 입력해주세요.");
+            int num2 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요.");
-        String str = sc.nextLine();
-        char c = str.charAt(0);
+            sc.nextLine();
 
+            System.out.print("사칙연산 기호를 입력하세요.");
+            String str = sc.nextLine();
+            char c = str.charAt(0);
 
-
-        if(c == '+'){
-        System.out.println("결과: " + (num1 + num2));
-        } else if (c == '-') {
-            System.out.println("결과: " + (num1 - num2));
-        } else if ((c == '*') && (num2 != 0)) {
-            System.out.println("결과: " + (num1 * num2));
-        } else if ((c == '/') && (num2 != 0)) {
-            System.out.println("결과: " + (num1 / num2));
-        } else {
-            System.out.println("나눗셈 연산에서 분모(두번째 숫자)에 0이 입력될 수 없습니다.");
+            if (c == '+') {
+                System.out.println("결과: " + (num1 + num2));
+            } else if (c == '-') {
+                System.out.println("결과: " + (num1 - num2));
+            } else if ((c == '*') && (num2 != 0)) {
+                System.out.println("결과: " + (num1 * num2));
+            } else if ((c == '/') && (num2 != 0)) {
+                System.out.println("결과: " + (num1 / num2));
+            } else {
+                System.out.println("나눗셈 연산에서 분모(두번째 숫자)에 0이 입력될 수 없습니다.");
+            }
+            System.out.println("계속 계산하시겠습니까?\n(그만 두시려면 exit를, 계속하시려면 아무키나 입력해주세요)");
+            str2 = sc.nextLine();
         }
 
     }
