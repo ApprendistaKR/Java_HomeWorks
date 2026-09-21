aff3bc3badcf6a3b0cf4f560f933ba8edbe6c9bf
ApprendistaKR <luke1475@naver.com>
2026-09-21 12:14:45 +0900

    코드 수정

--- calculator/src/main.java
+++ calculator/src/main.java
@@ -27,6 +27,7 @@ public class main {
 
             System.out.println("계속 계산하시겠습니까?\n(그만 두시려면 exit를, 오래된 결과 값을 삭제 하실려면 delet를, 전체 결과 값을 삭제 하실려면 all을, 계속하시려면 아무키나 입력해주세요)");
             str2 = sc.nextLine();
+
             if (str2.equals("all")){
                 cal.setList(new ArrayList<String>());
                 System.out.println("모든 연산 기록이 삭제되었습니다.");
