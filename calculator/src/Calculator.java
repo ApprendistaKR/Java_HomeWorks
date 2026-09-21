4d51383c830252d8a06247a8825b968c899fc8bf
ApprendistaKR <luke1475@naver.com>
2026-09-21 09:43:54 +0900

    setter 코드 수정

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -22,7 +22,8 @@ public class Calculator {
     public ArrayList<String> getList() {
         return list;
     }
-    public ArrayList<String> setList () {
-        return  list;
+    public void setList(ArrayList<String> list) {
+        this.list = list;
+    }
     }
 }
