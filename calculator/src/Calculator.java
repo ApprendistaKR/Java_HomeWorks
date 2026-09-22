73f5df875c29b14e8586bf2c30669492c7c0c184
ApprendistaKR <luke1475@naver.com>
2026-09-22 16:59:17 +0900

    사칙연산 관련 코드 수정

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -6,12 +6,33 @@ public class Calculator {
     OperatorType oper = null;
 
     String calculator(int num1, int num2, String str) {
-        switch (str) {
-            case "+" -> oper = OperatorType.PLUS;
-            case "-" -> oper = OperatorType.MINUS;
-            case "*" -> oper = OperatorType.MULTIPLY;
-            case "/" -> oper = OperatorType.DIVIDE;
+        if ((num1 <0)||(num2<0)){
+            return ("양의 정수를 입력해 주세요");
         }
+        switch (str) {
+            case "+":
+                oper = OperatorType.PLUS;{
+                    break;
+            }
+            case "-":
+                oper = OperatorType.MINUS;{
+                    break;
+            }
+            case "*":
+                oper = OperatorType.MULTIPLY;{
+                    break;
+            }
+            case "/":
+                oper = OperatorType.DIVIDE;{
+                if (num2 == 0) {
+                    return ("나눗셈 연산에서 분모(두번째 숫자)에는 0이 입력될 수 없습니다.");
+                }
+            break;
+
+                }
+        }
+
+
         result = String.valueOf(oper.Cal(num1, num2));
 //        if((num1 < 0) && (num2 < 0)) {
 //            this.result = ("음의 정수는 입력할 수 없습니다.");
@@ -29,16 +50,20 @@ public class Calculator {
         list.add(result);
         return result;
     }
+
     public ArrayList<String> getList() {
 
         return list;
     }
+
     public void setList(ArrayList<String> list) {
 
         this.list = list;
     }
-    public void removeResult(){
+
+    public void removeResult() {
 
         list.remove(0);
     }
 }
+
